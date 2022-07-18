3a6838659f22e13aba14f9216477098a0f763aee
Francois Viljoen <43622111+FDVILJOEN@users.noreply.github.com>
2022-07-18 12:09:58 +0100

    Hello World Added

---
+++ tests/helloworld/helloworld.js
@@ -0,0 +1,22 @@
+var hello = {
+    head: {
+        title: 'Hello World'
+    },
+    body: {
+        label: {
+            tag: 'label',
+            value: 'Please enter your name:'
+        },
+        txtUser: {
+            tag: 'input'
+        },
+        btnClickMe: {
+            tag: "button",
+            value: 'Click here',
+            onclick: function() {
+                alert('Hello ' + this.txtUser.value);
+            }
+
+        }
+    }
+}
